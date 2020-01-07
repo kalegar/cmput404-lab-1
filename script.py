87457a0845e2902b34f3e5575cd3eb857264c170
kalegar <atsmith@ualberta.ca>
2020-01-07 08:35:09 -0700

    update python script

--- script.py
+++ script.py
@@ -4,3 +4,6 @@ print(requests.__version__)
 
 r = requests.get('http://www.google.com')
 print(r)
+
+scr = requests.get('https://raw.githubusercontent.com/kalegar/cmput404-lab-1/master/script.py')
+print(scr.text)
